b42a76877b0abb1414640553b11ab8f463f0090f
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2022-11-19 20:19:15 -0800

    Wrong sketch file

--- controller/controller.ino
+++ controller/controller.ino
@@ -8,10 +8,8 @@ const byte val2 = 5;
 const byte val3 = 6;
 bool editing;
 bool home;
-bool v1;
-bool v2;
-bool v3;
 
+int currentVal;
 int currentScene;
 
 
@@ -19,46 +17,40 @@ void setup() {
 pinMode(select, INPUT_PULLUP);
 editing = false; 
 home = true;
-v1 = true;
+
 currentScene = 1;
+
 }
 
 void buttonCheck() {
  if(digitalRead(select) == LOW) switcher();
  
   if (digitalRead(edit) == LOW) {
-     
-      if (home) { editor(0); launchEdit(); }
-      else if (editing) {
+     if (editing) {
       Keyboard.press(KEY_ESC); 
       delay(100); 
       Keyboard.releaseAll();
       home = true; 
       editing = false;
       }
-
+    else if (home) { editor(0); launchEdit(); }
+      
   delay(1000);
     }
   
   if (digitalRead(val1) == LOW) {
       editor(1);
-      delay(200);
+      valSwitch(currentVal,currentScene, 1);
   }
   if (digitalRead(val2) == LOW) {
-      v1 = false; 
-      v2 = true; 
-      v3 = false; 
-      delay(200);
+     editor(2);
    }
   if (digitalRead(val3) == LOW) {
-      v1 = false; 
-      v2 = false; 
-      v3 = true;
-      delay(200);
+      editor(3);
   }
 }
 void launchEdit() {
-//Select source 1 in current scene
+//Select source 1 of current scene
     MouseTo.setTarget(350, 545);
     while (MouseTo.move() == false) {}
     delay(100);
@@ -73,7 +65,7 @@ void launchEdit() {
     editing = true; 
     home = false;
 
-//setup value editor
+//GO to value1 of current scene
 value1();
 }
 
@@ -102,14 +94,56 @@ void value1() {
     }
 }
 
+void valSwitch(int value, int scene, int targetVal) {
+  if (targetVal == value) {}
+  else if (targetVal == 1 && value == 3) value1();
+  else if (targetVal == 1 && value == 2) value1();
+  else if (scene == 1) {
+
+       if (targetVal == 2 && value == 3) { //move from 3 to 2
+          value1();
+          //tab tab...
+      }
+       if (targetVal == 5 && value == 2) { //move from 2 to 3
+          //tab tab...
+      }
+  } 
+  else if (scene == 2) {
+
+       if (targetVal == 2 && value == 3) { //move from 3 to 2
+          value1();
+          //tab tab...
+      }
+       if (targetVal == 5 && value == 2) { //move from 2 to 3
+          //tab tab...
+
+  }
+  else if (scene == 3) {
+
+       if (targetVal == 2 && value == 3) { //move from 3 to 2
+          value1();
+          //tab tab...
+      }
+       if (targetVal == 5 && value == 2) { //move from 2 to 3
+          //tab tab...
+  }
+  else if (scene == 4) {
+
+       if (targetVal == 2 && value == 3) { //move from 3 to 2
+          value1();
+          //tab tab...
+      }
+       if (targetVal == 5 && value == 2) { //move from 2 to 3
+          //tab tab...
+  }
+}
+
 void editor(int myVal) { 
- 
-   
+ myVal = currentVal;
       if (myVal == 1) { //8
         Keyboard.press(KEY_UP_ARROW);
         delay(1000);
         Keyboard.releaseAll();
-        
         myVal = 0;
       }
     
@@ -117,6 +151,7 @@ void editor(int myVal) {
 
 
 void switcher() {
+//Default settings
 
 //switch scenes
     Keyboard.press(KEY_RIGHT_ARROW);
